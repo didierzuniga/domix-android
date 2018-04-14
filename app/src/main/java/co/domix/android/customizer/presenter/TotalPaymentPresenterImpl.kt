cf2fd84c1549549cc6c127c4c6cebbd04fa74fd8
Didier Zúñiga <didier35@hotmail.com>
2018-04-13 22:08:28 -0500

    [add, update] Adding kt files and testing

---
+++ app/src/main/java/co/domix/android/customizer/presenter/TotalPaymentPresenterImpl.kt
@@ -0,0 +1,15 @@
+package co.domix.android.customizer.presenter
+
+/**
+ * Created by unicorn on 4/13/2018.
+ */
+class TotalPaymentPresenterImpl : TotalPaymentPresenter {
+
+    override fun queryOrderToPay(uid: String, payMethod: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun responseTotalToPayCash(commissionDomix: String, payTaxe: String, payTotalToDomix: String, minPayment: String, enableButtonPay: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}

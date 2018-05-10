6fa3717b3a5389c37ae1dea7fcbaf8c2ae787b84
Didier Zúñiga <didier35@hotmail.com>
2018-05-09 21:23:27 -0500

    [add] Added style_json.json for get map gray, but no used

--- app/src/main/java/co/domix/android/user/view/PickMap.java
+++ app/src/main/java/co/domix/android/user/view/PickMap.java
@@ -2,11 +2,13 @@ package co.domix.android.user.view;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -23,6 +25,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 import co.domix.android.R;
 
@@ -78,39 +81,26 @@ public class PickMap extends AppCompatActivity implements OnMapReadyCallback {
         });
     }
 
-//        mGoogleApiClient = new GoogleApiClient
-//                .Builder(this)
-//                .addApi(Places.GEO_DATA_API)
-//                .addApi(Places.PLACE_DETECTION_API)
-//                .enableAutoManage(this, this)
-//                .build();
-
-//        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-//        try {
-//            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-//        } catch (GooglePlayServicesRepairableException e) {
-//            e.printStackTrace();
-//        } catch (GooglePlayServicesNotAvailableException e) {
-//            e.printStackTrace();
-//        }
-
-//
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == PLACE_PICKER_REQUEST) {
-//            if (resultCode == RESULT_OK) {
-//                Place place = PlacePicker.getPlace(data, this);
-//                String toastMsg = String.format("Place: %s", place.getName());
-//                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-//            }
-//        }
-//    }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         double lat = Double.valueOf(shaPref.getString("latitude", ""));
         double lon = Double.valueOf(shaPref.getString("longitude", ""));
         LatLng myPos = new LatLng(lat, lon);
+
+//        Estas lineas para estilos de mapa
+//        try {
+//            boolean success = googleMap.setMapStyle(
+//                    MapStyleOptions.loadRawResourceStyle(
+//                            this, R.raw.style_json));
+//
+//            if (!success) {
+//                Log.e("jjj", "Style parsing failed.");
+//            }
+//        } catch (Resources.NotFoundException e) {
+//            Log.e("jjj", "Can't find style. Error: ", e);
+//        }
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPos, 15));
 
         googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
@@ -121,9 +111,4 @@ public class PickMap extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
     }
-
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-//
-//    }
 }
